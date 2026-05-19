f0ea263fb168f916dfa3973687c8d98fd36a7c3b
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-05-19 01:05:16 -0300

    fix: correct PostgreSQL host environment variable in helper module

--- src/db/postgres/helper.js
+++ src/db/postgres/helper.js
@@ -5,7 +5,7 @@ export const pool = new Pool({
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT,
-  host: process.env.POSTGRES_PORT,
+  host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
 }) //cria um poll no banco de dados com as variáveis de ambiente
 
