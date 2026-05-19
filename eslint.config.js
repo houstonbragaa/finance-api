97a8aff0b9cb17d916fc66c2244ae7f8085765a3
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-05-19 00:51:33 -0300

    chore: update eslint configuration to use node globals

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,15 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     //extensor de regras
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
     //regras do eslint
     rules: {},
   },
-]);
+])
