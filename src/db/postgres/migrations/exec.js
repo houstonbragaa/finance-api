516f4d3dbbe9604e170d38f8b373bbcef1ec9dd3
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-05-20 02:56:41 -0300

    refactor: enhance comments in migration execution script for clarity

--- src/db/postgres/migrations/exec.js
+++ src/db/postgres/migrations/exec.js
@@ -5,20 +5,20 @@ import path from 'path'
 import { readFileSync } from 'fs'
 
 const executeMigration = async () => {
-  const client = await pool.connect()
+  const client = await pool.connect() //conecta com o banco de dados postgres
 
   try {
-    const __filename = fileURLToPath(import.meta.url)
-    const __dirname = path.dirname(__filename)
+    const __filename = fileURLToPath(import.meta.url) //pega o filename
+    const __dirname = path.dirname(__filename) //pega o dirname
 
-    const filePath = path.join(__dirname, '01-init.sql')
-    const result = readFileSync(filePath).toString()
-    await client.query(result)
+    const filePath = path.join(__dirname, '01-init.sql') //pega o diretorio atual com o 01-init.sql
+    const result = readFileSync(filePath).toString() //pega o arquivo
+    await client.query(result) //sobe para o banco de dados
     console.log('migration executed successfully!')
   } catch (error) {
     console.log(error)
   } finally {
-    await client.release()
+    await client.release() //deixa atualizado
   }
 }
 
