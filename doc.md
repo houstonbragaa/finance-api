8666527d899a6f14157d07e68faed7382b81dd7d
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-05-22 01:51:16 -0300

    feat: add documentation and update Express routes for user management

---
+++ doc.md
@@ -0,0 +1,5 @@
+# Proximos passos
+
+- [x] colocar alguns dados do servidor express com variaveis de ambiente
+- [x] baixar o postman no browser
+- [] mudar os diretorios nas rotas express
--- index.js
+++ index.js
@@ -3,14 +3,18 @@ import express from 'express'
 import { helperPostgres } from './src/db/postgres/helper.js'
 
 const app = express() //crio um servidor com express
-const port = 3000 //defino a porta
 
-app.get('/', async (req, res) => {
+app.get('/api/users', async (req, res) => {
   const results = await helperPostgres.query('SELECT * FROM users') //chamo esse obj
 
   res.send(JSON.stringify(results)) //envio os resultados para o front-end
 })
 
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`)
+app.post('/api/users', async (req, res) => {
+  console.log(req.body) // req é um obj que tem esses métodos body, headers...
+  res.status(201).send('user created')
 })
+
+app.listen(process.env.EXPRESS_PORT, () => {
+  console.log(`Servidor rodando na porta ${process.env.EXPRESS_PORT}`)
+}) //aplicação disponível na porta 8080
