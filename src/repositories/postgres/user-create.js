986876df59e31cdc5492914d1f6e451d58ebaafc
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-05-28 01:41:43 -0300

    refactor: rename PostgresUserCreateRepository and update parameter naming for clarity

--- src/repositories/postgres/user-create.js
+++ src/repositories/postgres/user-create.js
@@ -1,7 +1,7 @@
 import { helperPostgres } from '../../db/postgres/helper'
 
-export class PostgresUserCreateReposiry {
-  async execute(userCreateParams) {
+export class PostgresCreateUserRepository {
+  async execute(createUserParams) {
     const results = await helperPostgres.query(
       `
       INSERT INTO users (ID, first_name, last_name, email, password)
@@ -9,11 +9,11 @@ export class PostgresUserCreateReposiry {
       RETURNING *
     `,
       [
-        userCreateParams.id,
-        userCreateParams.first_name,
-        userCreateParams.last_name,
-        userCreateParams.email,
-        userCreateParams.password,
+        createUserParams.id,
+        createUserParams.first_name,
+        createUserParams.last_name,
+        createUserParams.email,
+        createUserParams.password,
       ],
     )
 
