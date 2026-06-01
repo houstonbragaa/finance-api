80bb0aa501e277794ed0a7f66dd1777c7c6d13b1
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-06-01 01:17:23 -0300

    feat: update user password length in database schema and add migration script

--- src/db/postgres/migrations/01-init.sql
+++ src/db/postgres/migrations/01-init.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users(
   first_name VARCHAR(20) NOT NULL,
   last_name VARCHAR(20) NOT NULL,
   email VARCHAR(50) NOT NULL UNIQUE,
-  password VARCHAR(50) NOT NULL
+  password VARCHAR(100) NOT NULL
 );
 
 DO $$
