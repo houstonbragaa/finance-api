47d33c4955f1bc059c6be596d44be567b02df862
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-06-04 01:42:25 -0300

    feat: add validators password and email  into controller

--- src/controllers/create-user.js
+++ src/controllers/create-user.js
@@ -1,41 +1,40 @@
 import { CreateUserService } from '../services/create-user.js'
+import { badRequest, internalServerError, successCreate } from './helper.js'
+import validator from 'validator'
 
 export class CreateUserController {
   async execute(httpRequest) {
     try {
       const params = httpRequest.body
 
-      //validar campos obrigatórios e vazios
+      //validar campos
 
       const fieldsList = ['first_name', 'last_name', 'email', 'password']
 
       for (const field of fieldsList) {
-        if (!params[field] || params[field].trim().length === 0) {
-          return {
-            statusCode: 400,
-            body: {
-              errorMessage: `Missing field ${field}`,
-            },
-          }
+        const fieldIsValid = !params[field] || params[field].trim().length === 0
+        if (fieldIsValid) {
+          return badRequest({ errorMessage: `Missing field ${field}` })
         }
       }
+
+      const emailIsValid = validator.isEmail(params.email)
+      if (!emailIsValid) return badRequest({ errorMessage: 'Email Invalid!' })
+
+      const passwordIsValid = params.password.length < 8
+      if (passwordIsValid)
+        return badRequest({
+          errorMessage: 'The password must be at least 8 characters long!',
+        })
 
       //chamar o service (use-case)
 
       const service = new CreateUserService()
       const createdUser = await service.execute(params)
-      return {
-        statusCode: 201,
-        body: createdUser,
-      }
+      return successCreate(createdUser)
     } catch (error) {
       console.log(error)
-      return {
-        statusCode: 500,
-        body: {
-          errorMessage: 'Internal server error',
-        },
-      }
+      return internalServerError()
     }
   }
 }
