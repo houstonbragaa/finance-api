0f2fadc2370ecb7faafa7d62e32053c595db0e80
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-06-05 21:41:51 -0300

    fix: update import path helperPostgres

--- src/repositories/postgres/get-user-by-id.js
+++ src/repositories/postgres/get-user-by-id.js
@@ -1,6 +1,6 @@
-import { helperPostgres } from '../../db/postgres/helper'
+import { helperPostgres } from '../../db/postgres/helpers.js'
 
-export class PostgresGetUserById {
+export class PostgresGetUserByIdRepository {
   async execute(userId) {
     const user = await helperPostgres.query(
       'SELECT * FROM users WHERE id = $1',
