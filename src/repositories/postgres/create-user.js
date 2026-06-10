6abab113667505d2ba12ed4f9fe9e570e42b5c0b
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-06-10 00:28:13 -0300

    refactor: remove unused PostgresValidatorEmail class from create-user.js

--- src/repositories/postgres/create-user.js
+++ src/repositories/postgres/create-user.js
@@ -24,14 +24,3 @@ export class PostgresCreateUserRepository {
     return createdUser[0]
   }
 }
-
-export class PostgresValidatorEmail {
-  async execute(emailParams) {
-    const rows = await helperPostgres.query(
-      'SELECT id FROM users WHERE email = $1',
-      [emailParams],
-    )
-
-    return rows.length > 0
-  }
-}
