636614044008bb104c0601028a130702f4637198
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-06-10 03:07:55 -0300

    feat: throw UserNotFoundError when user is not found in GetUserByIdService

--- src/services/get-user-by-id.js
+++ src/services/get-user-by-id.js
@@ -1,9 +1,14 @@
+import { UserNotFoundError } from '../errors/user.js'
 import { PostgresGetUserByIdRepository } from '../repositories/postgres/get-user-by-id.js'
 
 export class GetUserByIdService {
   async execute(userId) {
     const repository = new PostgresGetUserByIdRepository()
     const user = await repository.execute(userId)
+
+    if (!user) {
+      throw new UserNotFoundError()
+    }
 
     return user
   }
