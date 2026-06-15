aec6911b411c8e7f1c8cbf2949c4cc7202575f15
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-06-15 03:30:49 -0300

    feat: add GetUsersAllController

---
+++ src/controllers/get-users-all.js
@@ -0,0 +1,21 @@
+import { NoOneUsersFoundError } from '../errors/user.js'
+import { GetUsersAllService } from '../services/get-users-all.js'
+import { internalServerError, notFound, ok } from './helpers.js'
+
+export class GetUsersAllController {
+  async execute() {
+    try {
+      const getUsersAllService = new GetUsersAllService()
+      const users = await getUsersAllService.execute()
+
+      return ok(users)
+    } catch (error) {
+      console.log(error)
+      if (error instanceof NoOneUsersFoundError) {
+        return notFound({ errorMessage: error.message })
+      }
+
+      return internalServerError()
+    }
+  }
+}
