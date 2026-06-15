be56f3b115cd934f29894c3dc4bc9637cda9884e
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-06-15 03:31:01 -0300

    feat: implement endpoint to retrieve all users using GetUsersAllController

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import { CreateUserController } from './src/controllers/create-user.js'
 import { GetUserByIdController } from './src/controllers/get-user-by-id.js'
+import { GetUsersAllController } from './src/controllers/get-users-all.js'
 
 const app = express() //crio um servidor com express
 
@@ -15,6 +16,12 @@ app.post('/api/users', async (req, res) => {
 
 app.get('/api/users/:userId', async (req, res) => {
   const controller = new GetUserByIdController()
+  const { statusCode, body } = await controller.execute(req)
+  res.status(statusCode).send(body)
+})
+
+app.get('/api/users', async (req, res) => {
+  const controller = new GetUsersAllController()
   const { statusCode, body } = await controller.execute(req)
   res.status(statusCode).send(body)
 })
