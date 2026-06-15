bc69ede6baaf7140b7506453910953156091f33b
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-06-15 03:27:03 -0300

    feat: add PostgresGetUserAllRepository

---
+++ src/repositories/postgres/get-user-all.js
@@ -0,0 +1,8 @@
+import { helperPostgres } from '../../db/postgres/helpers.js'
+
+export class PostgresGetUserAllRepository {
+  async execute() {
+    const result = await helperPostgres.query('SELECT * FROM users')
+    return result
+  }
+}
