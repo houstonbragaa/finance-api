5eec3f9da7f9680a1ac959d90e32597b3e0e6417
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-06-23 01:39:33 -0300

    feat: add updateUserService

---
+++ src/services/update-user.js
@@ -0,0 +1,39 @@
+import bcrypt from 'bcrypt'
+
+import { EmailAlreadyInUseError } from '../errors/user.js'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user'
+
+export class UpdateUserService {
+  async execute(userId, updateUserParams) {
+    // verificar se o email ja existe
+    if (updateUserParams.email) {
+      const emailRepository = new PostgresGetUserByEmailRepository()
+      const emailUserAlreadyExists = await emailRepository.execute(
+        updateUserParams.email,
+      )
+
+      if (emailUserAlreadyExists) {
+        throw new EmailAlreadyInUseError(updateUserParams.email)
+      }
+    }
+    //se mudar a senha, criptografa-lo
+
+    const user = {
+      ...updateUserParams,
+    }
+
+    if (updateUserParams.password) {
+      const passwordCrypted = await bcrypt.hash(updateUserParams.password, 10)
+
+      user.password = passwordCrypted
+    }
+
+    //chamar o repository
+
+    const updateUserRepository = new PostgresUpdateUserRepository()
+    const result = await updateUserRepository.execute(userId, user)
+
+    return result
+  }
+}
