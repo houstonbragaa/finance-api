5f7f0f9f653503577929cec8ddae17e6c1a6eb8d
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-07-15 02:11:10 -0300

    feat: add DELETE endpoint for user deletion in the API

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { CreateUserController } from './src/controllers/create-user.js'
 import { GetUserByIdController } from './src/controllers/get-user-by-id.js'
 import { GetUsersAllController } from './src/controllers/get-users-all.js'
 import { UpdateUserController } from './src/controllers/update-user.js'
+import { DeleteUserController } from './src/controllers/delete-user.js'
 
 const app = express() //crio um servidor com express
 
@@ -29,6 +30,12 @@ app.patch('/api/users/:userId', async (req, res) => {
 
 app.get('/api/users', async (req, res) => {
   const controller = new GetUsersAllController()
+  const { statusCode, body } = await controller.execute(req)
+  res.status(statusCode).send(body)
+})
+
+app.delete('/api/users/:userId', async (req, res) => {
+  const controller = new DeleteUserController()
   const { statusCode, body } = await controller.execute(req)
   res.status(statusCode).send(body)
 })
