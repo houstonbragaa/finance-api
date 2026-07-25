752ebb0ac4617f65b2e53fa2e7fed7ddc39700d2
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-07-25 00:24:38 -0300

    feat: consolidate user-related exports in Postgres and services directories

---
+++ src/repositories/postgres/index.js
@@ -0,0 +1,6 @@
+export * from './create-user.js'
+export * from './delete-user.js'
+export * from './get-user-all.js'
+export * from './get-user-by-email.js'
+export * from './get-user-by-id.js'
+export * from './update-user.js'
---
+++ src/services/index.js
@@ -0,0 +1,5 @@
+export * from './create-user.js'
+export * from './delete-user.js'
+export * from './get-user-by-id.js'
+export * from './get-users-all.js'
+export * from './update-user.js'
